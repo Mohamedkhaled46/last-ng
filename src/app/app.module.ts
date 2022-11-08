d74f498c8671b27652dc546e647460a305f9c569
Mohamedkhaled46 <mookhaled46@gmail.com>
2022-11-09 01:50:37 +0200

    =momomo

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+
+import { ContactComponent } from './contact/contact.component';
+import { ClientComponent } from './client/client.component';
+import { ProjectComponent } from './project/project.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServiceComponent,
+    ContactComponent,
+    ClientComponent,
+    ProjectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
